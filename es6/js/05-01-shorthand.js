6e52cdc8b33e85d57ce8e8a0bb39bc525f1c4a14
Thiago Aires <thiago@MAC-016.local>
2017-09-15 14:40:56 -0300

    Capitulo 5

---
+++ es6/js/05-01-shorthand.js
@@ -0,0 +1,18 @@
+let firstName = 'thiago';
+let lastName =  'silva';
+let age =       33;
+
+const person = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age
+}
+// o shorthand properties do ES6 permite que: se a propriedade e a variavel tiverem o mesmo nome, basta deixar apenas a variavel
+
+const personShorthand = {
+    firstName,
+    lastName,
+    age
+}
+
+console.log(personShorthand);
