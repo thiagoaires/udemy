3261bfca12a00bb9d68aa89fdc5056fb23555950
Thiago Aires <makarrao@gmail.com>
2017-09-16 18:58:27 -0300

    final - aula 5

---
+++ es6/js/05-03-metodos-string.js
@@ -0,0 +1,6 @@
+let text = 'lorem ipsum dolor ai que dor';
+
+console.log(text.startsWith('rem', 2));
+console.log(text.endsWith('dor'));
+console.log('Hue'.repeat(10));
+console.log(text.includes('ipsum'));
