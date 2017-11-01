ffcf5fc9bdab36d0ee5df59b29c76be1a7f3148e
Thiago Aires <thiago.aires@loopix.sa>
2017-11-01 16:43:26 -0200

    capitulo 24

--- Angular4/Typescript/jedi-academy/src/app/app.component.ts
+++ Angular4/Typescript/jedi-academy/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { Student } from './student/student.model'
+
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jad';
+
+  students: Student[] = [
+    { name: 'Luke Skywalker', isJedi: true, temple: 'Coruscant' },
+    { name: 'Leia Organa', isJedi: false },
+    { name: 'Han Solo', isJedi: false }
+  ]
 }
