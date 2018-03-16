ce6c99dc8c7fb203ea2b77e7d65fe081678a435c
Thiago Aires [note] <makarrao@gmail.com>
2018-03-15 21:49:21 -0300

    NodeJS - Sec4-19

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,17 +1,18 @@
 let express = require('express');
+let msg = require('./mod_teste')();
 let app = express();
 
 //informar ao express que o modulode view eh o ejs
 app.set('view engine', 'ejs');
 
-app.get('/tecnologia', (req, res) => res.render('secao/tecnologia') );
+app.get('/', (req, res) => res.render('home/index'));
 
-app.get('/', (req, res) => {
 
-    res.send("<html><body>Portal Noticias</body></htnml>");
-});
+app.get('/formulario_inclusao_noticia', (req, res) => res.render('admin/form_add_noticia.ejs'));
 
-app.listen(3000, () =>{
+app.get('/noticias', (req, res) => res.render('noticias/noticias'))
 
-    console.log('server ok');
+app.listen(3000, () => {
+
+    console.log(msg);
 });
---
+++ nodejs/mod_teste.js
@@ -0,0 +1,5 @@
+let msg = 'este modulo contem apenas 1 string';
+
+module.exports = function(){
+    return msg
+};
