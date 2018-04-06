a1f7aafe55b4f3a991fbd7e25dcedf217b7a21b2
Thiago Aires <thiago.aires@loopix.com.br>
2018-04-06 15:51:25 -0300

    react da2k - sec3-23

--- da2k/reactjs/react-webpack/src/app.js
+++ da2k/reactjs/react-webpack/src/app.js
@@ -1,13 +1,15 @@
 'use strict'
 
 import React, {Component} from 'react'
-import Title from './title'
+import Square from './square'
 
 class App extends Component {
   render () {
     return (
       <div className='container'>
-        <Title name='Thiagao' lastname='Aires' />
+        {['blue', 'red', 'green'].map((square, indice) => (
+          <Square key={indice} color={square} />
+        ))}
       </div>
     )
   }
