406fd369ea9e1ccaae392878f235b41c05c6ae0f
Thiago Aires [note] <makarrao@gmail.com>
2018-04-08 14:32:50 -0300

    react da2k - sec3-29

--- da2k/reactjs/react-webpack/src/app.js
+++ da2k/reactjs/react-webpack/src/app.js
@@ -1,15 +1,15 @@
 'use strict'
 
 import React, {Component} from 'react'
-import Button from './button'
+import LikeButton from './like-button'
+import SearchButton from './search-button'
 
 class App extends Component {
   render () {
     return (
-      <div className='container'>
-        <Button>
-          <i>Botao negrito</i>
-        </Button>
+      <div>
+        <LikeButton />
+        <SearchButton />
       </div>
     )
   }
---
+++ da2k/reactjs/react-webpack/src/like-button.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import React from 'react'
+import Button from './button'
+
+const LikeButton = () => (
+  <Button handleClick={() => (alert('curti'))}>Curtir</Button>
+)
+
+export default LikeButton
