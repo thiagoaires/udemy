2fa25cb8a086a0e9f3f7977af2d77894a3857f6b
Thiago Aires [note] <makarrao@gmail.com>
2018-04-08 00:32:54 -0300

    react da2k - sec3-28

--- da2k/reactjs/react-webpack/src/app.js
+++ da2k/reactjs/react-webpack/src/app.js
@@ -1,13 +1,15 @@
 'use strict'
 
 import React, {Component} from 'react'
-import Square from './square'
+import Button from './button'
 
 class App extends Component {
   render () {
     return (
-      <div className='container' onClick={(e) => { alert('clicou') }}>
-        <Square />
+      <div className='container'>
+        <Button>
+          <i>Botao negrito</i>
+        </Button>
       </div>
     )
   }
