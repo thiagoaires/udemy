d7f6a17bf875f821a3db6262485b191cf0076370
Thiago Aires <thiago.aires@loopix.com.br>
2018-04-24 11:08:24 -0300

    react m1-35 [githut app{Marcaçao}]

--- da2k/reactjs/react-webpack/src/app.js
+++ da2k/reactjs/react-webpack/src/app.js
@@ -1,33 +1,42 @@
 'use strict'
 
-import React, { Component } from 'react'
+import React from 'react'
 
-class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      checked: false,
-      showContent: false
-    }
-  }
-  render () {
-    return (
-      <div>
-        <input 
-          type='checkbox'
-          checked={this.state.checked}
-          onChange={() => {
-            this.setState({
-              checked: !this.state.checked
-            }, () => {
-              this.setState({showContent: this.state.checked})
-            })
-          }}
-        /> mostrar conteudo
-        {this.state.showContent && <div> olha eu aqui</div>}
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className='app'>
+    <div className='search'>
+      <input type='search' placeholder='Digite o nome do usuário no GitHub' />
+    </div>
+    <div className='user-info'>
+      <img src='https://avatars3.githubusercontent.com/u/625627?v=4' />
+      <h1><a href='https://github.com/thiagoaires'>Thiago Aires</a></h1>
+    </div>
+
+    <ul className='repos-info'>
+      <li>- Repositorios: 5</li>
+      <li>- Seguidores: 0</li>
+      <li>- Seguindo: 3</li>
+    </ul>
+
+    <div className='actions'>
+      <button>Ver Repositorios</button>
+      <button>Ver Favoritos</button>
+    </div>
+
+    <div className='repos'>
+      <h2>Repositorios</h2>
+      <ul>
+        <li><a href='#'>Nome do repositorio</a></li>
+      </ul>
+    </div>
+
+    <div className='starred'>
+      <h2>Favoritos</h2>
+      <ul>
+        <li><a href='#'>Lista de Favoritos</a></li>
+      </ul>
+    </div>
+  </div>
+)
 
 export default App
