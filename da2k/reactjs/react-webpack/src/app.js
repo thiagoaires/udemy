8de72b1cae64d7d3ccb31fcaaef0878f44e475ff
Thiago Aires [note] <makarrao@gmail.com>
2018-04-26 01:52:23 -0300

    React: m1-36

--- da2k/reactjs/react-webpack/src/app.js
+++ da2k/reactjs/react-webpack/src/app.js
@@ -1,43 +1,33 @@
 'use strict'
 
 import React from 'react'
+import Search from './components/search'
+import UserInfo from './components/user-info'
+import Actions from './components/actions'
+import Repos from './components/repos'
 
 const App = () => (
   <div className='app'>
-    <div className='search'>
-      <input type='search' placeholder='Digite o nome do usuário no GitHub' />
-    </div>
-    <div className='user-info'>
-      <img src='https://avatars3.githubusercontent.com/u/625627?v=4' />
-      <h1 className='username'><a href='https://github.com/thiagoaires'>Thiago Aires</a></h1>
-    </div>
+    <Search />
+    <UserInfo />
 
-    <ul className='repos-info'>
-      <li>- Repositorios: 5</li>
-      <li>- Seguidores: 0</li>
-      <li>- Seguindo: 3</li>
-    </ul>
-
-    <div className='actions'>
-      <button>Ver Repositorios</button>
-      <button>Ver Favoritos</button>
-    </div>
-
-    <div className='repos'>
-      <h2>Repositorios</h2>
-      <ul>
-        <li><a href='#'>Nome do repositorio</a></li>
-        <li><a href='#'>Nome do repositorio</a></li>
-      </ul>
-    </div>
-
-    <div className='starred'>
-      <h2>Favoritos</h2>
-      <ul>
-        <li><a href='#'>Lista de Favoritos</a></li>
-        <li><a href='#'>Lista de Favoritos</a></li>
-      </ul>
-    </div>
+    <Actions />
+    <Repos
+      className='repos'
+      title='Repositórios:'
+      repos={[{
+        name: 'nome do repositorio',
+        link: '#'
+      }]}
+    />
+    <Repos
+      className='starred'
+      title='Favorios:'
+      repos={[{
+        name: 'nome do favorito',
+        link: '#'
+      }]}
+    />
   </div>
 )
 
---
+++ da2k/reactjs/react-webpack/src/components/user-info.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import React from 'react'
+
+const UserInfo = () => (
+  <div className='user-info'>
+    <img src='https://avatars3.githubusercontent.com/u/625627?v=4' />
+
+    <h1 className='username'>
+      <a href='https://github.com/thiagoaires'>Thiago Aires</a>
+    </h1>
+
+    <ul className='repos-info'>
+      <li>- Repositorios: 5</li>
+      <li>- Seguidores: 0</li>
+      <li>- Seguindo: 3</li>
+    </ul>
+
+  </div>
+)
+
+export default UserInfo
