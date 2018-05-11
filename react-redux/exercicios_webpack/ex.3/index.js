59fd6cea5ec316ed259a14750a156ec9475fdc33
Thiago Aires <thiago.aires@MAC-016.local>
2018-05-11 13:06:56 -0300

    React-redux - Fim modulo webpack \o/

---
+++ react-redux/exercicios_webpack/ex.3/index.js
@@ -0,0 +1,4 @@
+import Pessoa from './pessoa'
+
+const pessoa = new Pessoa('Guilherme 2')
+console.log(pessoa.toString())
---
+++ react-redux/exercicios_webpack/ex/index.js
@@ -0,0 +1,10 @@
+import './estilo.css'
+import 'react'
+
+export default props => (
+  <div>
+    <h1>oi</h1>
+  </div>
+)
+
+console.log('funcionou')
