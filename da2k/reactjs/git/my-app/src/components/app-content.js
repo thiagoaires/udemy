ed0e09b15f4c63c2733ac97451f123f6fb1545ec
Thiago Aires <thiago.aires@MAC-016.local>
2018-05-23 17:08:39 -0300

    React modulo 2 aulo 38

--- da2k/reactjs/git/my-app/src/components/app-content.js
+++ da2k/reactjs/git/my-app/src/components/app-content.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Search from './search'
+import Search from './search/'
 import UserInfo from './user-info'
 import Actions from './actions'
 import Repos from './repos'
