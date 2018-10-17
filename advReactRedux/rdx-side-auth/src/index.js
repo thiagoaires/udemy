1e2b963af945aa88de74ef42a50fd5fdb77e50f1
Thiago Aires <thiago.aires@MAC-016.local>
2018-10-17 17:03:45 -0300

    Redux form

--- advReactRedux/rdx-side-auth/src/index.js
+++ advReactRedux/rdx-side-auth/src/index.js
@@ -7,16 +7,21 @@ import App from './components/App'
 
 import Welcome from './components/Welcome'
 import Signup from './components/auth/Signup'
+import reducers from './components/reducers'
 
 import { createStore } from 'redux'
-
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route exact path='/' component={Welcome} />
-      <Route path='/registro' component={Signup} />
-    </App>
-  </BrowserRouter>,
+  <Provider store={createStore(reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  }>
+    <BrowserRouter>
+      <App>
+        <Route exact path='/' component={Welcome} />
+        <Route path='/registro' component={Signup} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#root')
 )
