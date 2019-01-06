db37cc972e642ab2fd8e4c23e32fd568bb306613
Thiago Aires <makarrao@gmail.com>
2019-01-06 10:15:25 -0200

    webpack ok

--- es6/js/14/app.js
+++ es6/js/14/app.js
@@ -1,8 +1,11 @@
-import * as R from 'ramda'
+// import * as R from 'ramda'
+import { uniq, union} from 'ramda'
 
-const arr1 = [1, 2, 3, 4, 5, 6, 7,6, 6, 1]
-const arr2 = [1, 2, 3, 4, 5, 6, 7,6, 6, 1]
+const arr1 = [1, 2, 3, 4, 5, 6, 76,5, 4, 1]
+const arr2 = [1, 4, 5, 6, 1, 40, 22, 12]
 
-const arr3 = R.union(arr1, arr2)
+const arr3 = union(arr1, arr2)
 
-console.log(arr3)
+const arr4 = uniq(arr1)
+
+console.log(arr3, arr4)
